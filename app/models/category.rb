5d1570212fd853644a7a7a2624631f85135e7590
DaichiSaito <hrkedz@gmail.com>
2020-05-21 12:13:56 +0900

    [fix]アソシエーションの設定足りてなかった

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,5 @@
 class Category < ActiveRecord::Base
   self.table_name = "category"
   has_many :film_categories
+  has_many :films, through: :film_categories
 end
--- app/models/film.rb
+++ app/models/film.rb
@@ -1,7 +1,9 @@
 class Film < ActiveRecord::Base
   self.table_name = "film"
   has_many :film_actors
+  has_many :actors, through: :film_actors
   has_many :film_categories
+  has_many :categories, through: :film_categories
   has_many :inventories
   belongs_to :language
   belongs_to :original_language, class_name: 'Language'
